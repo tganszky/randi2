56460d776065de9a63d23443136798b05208926b
Thomas Ganszky <tg@Greyhound.(none)>
2010-08-27 22:56:38 +0200

    before merging

--- src/main/java/de/randi2/install/domain/Configuration.java
+++ src/main/java/de/randi2/install/domain/Configuration.java
@@ -24,100 +24,35 @@ import de.randi2.utility.validations.EMailRANDI2;
 import de.randi2.utility.validations.TelephonNumber;
 
 @Data
-@EqualsAndHashCode(callSuper=true)
-public class Configuration extends AbstractDomainObject{
-	
+@EqualsAndHashCode(callSuper = true)
+public class Configuration extends AbstractDomainObject {
+
 	private static final long serialVersionUID = 1185826192804499840L;
 
 	@NotEmpty
 	private String key;
 	@NotEmpty
 	private String value;
-	
-	/** The Constant MAX_NAME_LENGTH. */
-	public final static int MAX_NAME_LENGTH = 50;
-	
-	/** The Constant MAX_TITLE_LENGTH. */
-	public final static int MAX_TITLE_LENGTH = 20;
-	
-	// Persons Data
-	/** The surname. */
-	@NotEmpty
-	@Length(max=MAX_NAME_LENGTH)
-	private String surname = "";
-	
-	/** The firstname. */
-	@NotEmpty
-	@Length(max=MAX_NAME_LENGTH)
-	private String firstname = "";
-	
-	/** The title. */
-	@Length(max=MAX_TITLE_LENGTH)
-	private String title = "";
 
-	// Contact Data
-	/** The email. */
-	@EMailRANDI2
-	@NotEmpty
-	private String email = "";
-
-	/** The phone. */
-	@TelephonNumber
-	@NotEmpty
-	private String phone = "";
-	
-	/** The mobile. */
-	@TelephonNumber
-	private String mobile = "";
-
-	/** The fax. */
-	@TelephonNumber
-	private String fax = "";
-	
-	// Institutional Data
-	/** The assistant. */
-	@ManyToOne(cascade=CascadeType.ALL)
-	@ContactPerson
-	private Person assistant;
-	
-	/** The trial site. */
-	@ManyToOne
-	private TrialSite trialSite;
-
-	// Login data
-	/** The login. */
-	@OneToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER, mappedBy="person")
+	/** The Login of the first user. */
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "person")
 	private Login login;
 
-	
-	//Installation Data
-	@versionnumber
-	private String versionnumber
-	
-	//mail.host=localhost
-	//mail.username=randi2
-	//mail.password=randi2
-	//mail.from=randi2@randi2.de
-	//info.server=http://randi2.org
-	//info.hoster=RANDI2 Team
-	//
-	//
-	//
-	//randi2.properties
-	//logoPath=/hostingInstLogo.png
-	//sysopMail=ds@randi2.de
-	//version=
-	//website1=http\://www.randi2.de
-	//website2=http\://www.dkfz.de
-	//year=2010
-	//selfRegistration=true
-	
-	
-	/* (non-Javadoc)
-	 * @see de.randi2.model.AbstractDomainObject#getUIName()
-	 */
-	@Override
-	public String getUIName() {
-		return this.getSurname()+", "+this.getFirstname();
-	}
+	private String mail_host;
+	private String mail_username;
+	private String mail_password;
+	private String mail_from;
+	private String info_server;
+	private String info_hoster;
+
+	// randi2.properties
+	// logoPath=/hostingInstLogo.png
+	private String sysopMail;
+	private String randi2_version;
+	private String website1;
+	private String website2;
+	// year=2010
+	// selfRegistration=true
+	private String abouttext;
+	private String tst;
 }
